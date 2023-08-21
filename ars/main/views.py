dcad4ea5e0a2bf569dcfeb0a00252c56652768b3
IlgarDesktop <ilgar.bagishev@gmail.com>
2023-08-21 17:02:26 +0300

    added project page

--- ars/main/views.py
+++ ars/main/views.py
@@ -22,4 +22,15 @@ def project(request, pk):
     images = []
     for i in object_item.images.all():
         images.append(i)
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        number = request.POST.get("number")
+        if name and number:
+            message = Message()
+            message.name = name
+            message.number = number
+            message.save()
+            send_mail(f"Имя: {name}         Номер: {number}", "Сайт АРС отправил эту форму", "sunclub.stor@gmail.com", ["elkhan.bagishev2003@gmail.com"])
+
     return render(request, "project.html", {"object": object_item, "images": enumerate(images)})
